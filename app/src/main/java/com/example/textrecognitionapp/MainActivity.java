8b2f414b3d4681620d8566eac623c606c48185ff
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2021-11-22 17:30:49 +0800

    Commit #3: Text recognition feature
    Added text recognition feature using ML Kit. Able to extract text from image.
    Issues found: Picture saved on imageView in low quality, resulting in inaccurate text extraction.

--- app/src/main/java/com/example/textrecognitionapp/MainActivity.java
+++ app/src/main/java/com/example/textrecognitionapp/MainActivity.java
@@ -4,12 +4,23 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     private Button captureImageBtn, extractTextBtn;
     private ImageView capturedImage;
     private TextView extractedText;
+    private Bitmap imageBitmap;
+    private String tempText;
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
@@ -30,12 +43,17 @@ public class MainActivity extends AppCompatActivity {
         captureImageBtn = findViewById(R.id.captureImageBtn);
         extractTextBtn = findViewById(R.id.extractTextBtn);
 
+        // Calls camera activity function when 'Take Picture' button is pressed
         captureImageBtn.setOnClickListener(v -> {
             dispatchTakePictureIntent();
         });
+
+        extractTextBtn.setOnClickListener(v -> {
+            extractTextFromImage();
+        });
     }
 
-
+    // Function for launching camera activity to capture image
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         try {
@@ -48,11 +66,38 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // When previous activity was from 'dispatchTakePictureIntent' function
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageBitmap = (Bitmap) extras.get("data");
             capturedImage.setImageBitmap(imageBitmap);
             Toast.makeText(getApplicationContext(), "Image captured successfully!", Toast.LENGTH_SHORT).show();
+            extractedText.setText("");
+        }
+    }
+
+    // Function to extract text from image
+    private void extractTextFromImage() {
+        FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(imageBitmap);
+        FirebaseVisionTextDetector firebaseVisionTextDetector = FirebaseVision.getInstance().getVisionTextDetector();
+        firebaseVisionTextDetector.detectInImage(firebaseVisionImage).addOnSuccessListener(firebaseVisionText -> {
+            displayImageText(firebaseVisionText);
+        }).addOnFailureListener(e -> { // Failure listener
+            Toast.makeText(getApplicationContext(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.d("ERROR", e.getMessage());
+        });
+    }
+
+    private void displayImageText(FirebaseVisionText firebaseVisionText) {
+        List<FirebaseVisionText.Block> blockList = firebaseVisionText.getBlocks();
+        if (blockList.size() == 0) {
+            Toast.makeText(getApplicationContext(), "Error: No text found in image provided!", Toast.LENGTH_SHORT).show();
+        } else {
+            for (FirebaseVisionText.Block block: firebaseVisionText.getBlocks()) {
+                String currentText = extractedText.getText().toString();
+                tempText = block.getText();
+                extractedText.setText(currentText + tempText);
+            }
         }
     }
 }
